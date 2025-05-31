429acbe715dac72b712b1ddb87ca1cffbe1b803e
Eric Zhang <ericbkzhang@gmail.com>
2025-05-31 10:38:10 -0400

    Add PyTorch and split images into left and right

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+import glob
+
 import cv2
+import torch
+import torchvision
+
+left_hand = [cv2.imread(file) for file in glob.glob("img/left/*.jpg")]
+print(left_hand)
+
+right_hand = [cv2.imread(file) for file in glob.glob("img/right/*.jpg")]
+print(right_hand)
