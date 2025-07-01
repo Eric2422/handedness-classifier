9483d6e18c9020a5c580adfe7bf5087a1487edf0
Eric Zhang <ericbkzhang@gmail.com>
2025-06-30 23:47:17 -0400

    Update requirements

--- src/main.py
+++ src/main.py
@@ -2,16 +2,15 @@ import glob
 import pathlib
 
 import cv2
-import torch
-import torchvision
+import skimage
 
 # List of accepted image file extensions
 ACCEPTED_EXTENSIONS = ('.jpg', '.jpeg', '.png')
 
-# Find all the images of left-handed writing
+# Get all the images of left-handed writing
 left_hand_images = [cv2.imread(file) for file in glob.glob(
     './img/left/*') if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
 
-# Find all images of right-handed writing
+# Get all images of right-handed writing
 right_hand_images = [cv2.imread(file) for file in glob.glob(
     './img/right/*') if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
