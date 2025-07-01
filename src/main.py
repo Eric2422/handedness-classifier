e0f653889ed54e1e81225a89cc2257cc0a8586a8
Eric Zhang <ericbkzhang@gmail.com>
2025-07-01 18:13:30 -0400

    Update libraries yet again

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
-import glob
+import os
 import pathlib
 
 import cv2
-import skimage
+import natsort
+import skimage as ski
 
 # List of accepted image file extensions
 ACCEPTED_EXTENSIONS = ('.jpg', '.jpeg', '.png')
 
 # Get all the images of left-handed writing
-left_hand_images = [cv2.imread(file) for file in glob.glob(
-    './img/left/*') if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
+left_hand_files = [file for file in natsort.natsorted(os.listdir(
+    './img/left')) if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
+print(left_hand_files)
 
 # Get all images of right-handed writing
-right_hand_images = [cv2.imread(file) for file in glob.glob(
-    './img/right/*') if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
+right_hand_files = [file for file in natsort.natsorted(os.listdir(
+    './img/right')) if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
+print(right_hand_files)
