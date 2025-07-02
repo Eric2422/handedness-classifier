d60f81cc4dcfef66ac50737895a9ce0fa75d91ce
Eric Zhang <ericbkzhang@gmail.com>
2025-07-02 10:54:33 -0400

    Add skimage image reading

--- src/main.py
+++ src/main.py
@@ -5,15 +5,21 @@ import cv2
 import natsort
 import skimage as ski
 
+IMG_DIRECTORY = './img'
+
 # List of accepted image file extensions
 ACCEPTED_EXTENSIONS = ('.jpg', '.jpeg', '.png')
 
 # Get all the images of left-handed writing
-left_hand_files = [file for file in natsort.natsorted(os.listdir(
-    './img/left')) if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
-print(left_hand_files)
+left_files = [file for file in natsort.natsorted(os.listdir(
+    f'{IMG_DIRECTORY}/left')) if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
+print(left_files)
+
+left_images = [ski.io.imread(f'{IMG_DIRECTORY}/left/{file}') for file in left_files]
 
 # Get all images of right-handed writing
-right_hand_files = [file for file in natsort.natsorted(os.listdir(
-    './img/right')) if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
-print(right_hand_files)
+right_files = [file for file in natsort.natsorted(os.listdir(
+    f'{IMG_DIRECTORY}/right')) if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
+print(right_files)
+
+right_images = [ski.io.imread(f'{IMG_DIRECTORY}/right/{file}') for file in right_files]
