1ceb41a44b342f59f59970ed801eae2058fb6e32
Eric Zhang <ericbkzhang@gmail.com>
2025-07-02 18:22:50 -0400

    Use os.Path objects

--- src/main.py
+++ src/main.py
@@ -5,21 +5,24 @@ import cv2
 import natsort
 import skimage as ski
 
-IMG_DIRECTORY = './img'
+IMG_DIRECTORY = pathlib.Path('./img')
+LEFT_DIRECTORY = IMG_DIRECTORY / 'left'
+RIGHT_DIRECTORY = IMG_DIRECTORY / 'right'
 
 # List of accepted image file extensions
 ACCEPTED_EXTENSIONS = ('.jpg', '.jpeg', '.png')
 
 # Get all the images of left-handed writing
 left_files = [file for file in natsort.natsorted(os.listdir(
-    f'{IMG_DIRECTORY}/left')) if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
-print(left_files)
+    LEFT_DIRECTORY)) if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
 
-left_images = [ski.io.imread(f'{IMG_DIRECTORY}/left/{file}') for file in left_files]
+left_images = [ski.io.imread(LEFT_DIRECTORY / file) for file in left_files]
+print(left_images)
 
 # Get all images of right-handed writing
 right_files = [file for file in natsort.natsorted(os.listdir(
-    f'{IMG_DIRECTORY}/right')) if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
+    RIGHT_DIRECTORY)) if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
 print(right_files)
 
-right_images = [ski.io.imread(f'{IMG_DIRECTORY}/right/{file}') for file in right_files]
+right_images = [ski.io.imread(RIGHT_DIRECTORY / file) for file in right_files]
+print(right_images)
