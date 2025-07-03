8d4e1a59069245718d304178ad3ec51a7885e801
Eric Zhang <ericbkzhang@gmail.com>
2025-07-03 15:02:27 -0400

    Split data into test and train, and use RandomForestClassifier rather than DecisionTreeClassifier

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ import os
 import pathlib
 
 import natsort
-import numpy as np
 import skimage as ski
-import sklearn.tree
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
 
 IMG_DIRECTORY = pathlib.Path('./img')
 LEFT_DIRECTORY = IMG_DIRECTORY / 'left'
@@ -18,19 +19,21 @@ left_files = [file for file in natsort.natsorted(os.listdir(
     LEFT_DIRECTORY)) if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
 
 left_images = [ski.io.imread(LEFT_DIRECTORY / file) for file in left_files]
-# print(left_images)
 
 # Get all images of right-handed writing
 right_files = [file for file in natsort.natsorted(os.listdir(
     RIGHT_DIRECTORY)) if pathlib.Path(file).suffix.lower() in ACCEPTED_EXTENSIONS]
 
 right_images = [ski.io.imread(RIGHT_DIRECTORY / file) for file in right_files]
-# print(right_images)
 
 x_data = [img for img in left_images + right_images]
 print(x_data)
 
 y_data = [0] * len(left_images) + [1] * len(right_images)
 
-clf = sklearn.tree.DecisionTreeClassifier()
-clf = clf.fit(x_data, y_data)
+x_train, x_test, y_train, y_test = train_test_split(
+    x_data, y_data
+)
+
+clf = RandomForestClassifier()
+clf = clf.fit(x_train, y_train)
