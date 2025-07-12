cad16935d92970f940be874db1304d9a8b3fc8a9
Eric Zhang <ericbkzhang@gmail.com>
2025-07-11 20:23:33 -0400

    Add OAuth, update `praw.ini.sample`, and information about setting up `praw.ini` to the README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ambidexterity-test
+
 A small computer vision program to try and distinguish left- and right-handed handwriting.
+
+## `praw.ini`
+
+When you clone this repo, copy the contents of [`praw.ini`](./praw.ini) 
+into a new file called `./praw.ini` to configure the Reddit crawler.
+Then fill in the empty fields with the necessary information.
--- src/reddit_api.py
+++ src/reddit_api.py
@@ -1,11 +1,19 @@
+import configparser
 import time
 
 import praw
 import pathlib
 
-CONFIG_FILE = pathlib.Path('./praw.ini')
 
-reddit = praw.Reddit(
-    site_name='bot',
-    
+CONFIG = configparser.ConfigParser()
+CONFIG.read('./praw.ini')
+
+REDDIT = praw.Reddit(
+    client_id=CONFIG['DEFAULT']['client_id'],
+    client_secret=CONFIG['DEFAULT']['client_secret'],
+    password=CONFIG['DEFAULT']['password'],
+    user_agent=CONFIG['DEFAULT']['user_agent'],
+    username=CONFIG['DEFAULT']['username'],
 )
+
+print(REDDIT.user.me())
