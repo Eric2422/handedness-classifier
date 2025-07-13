e350adfc94089d239bc971084391443076b6c826
Eric Zhang <ericbkzhang@gmail.com>
2025-07-13 09:09:31 -0400

    Add subreddit searching

--- src/reddit_api.py
+++ src/reddit_api.py
@@ -2,7 +2,6 @@ import configparser
 import time
 
 import praw
-import pathlib
 
 
 CONFIG = configparser.ConfigParser()
@@ -16,4 +15,22 @@ REDDIT = praw.Reddit(
     username=CONFIG['DEFAULT']['username'],
 )
 
-print(REDDIT.user.me())
+SUBREDDITS = ("handwriting",)
+"""List of subreddits to search through."""
+
+KEY_WORDS = ('ambidextrous', 'left hand', 'right hand')
+"""List of key words to search with."""
+
+(REDDIT.subreddit(subreddit).search() for subreddit in SUBREDDITS)
+for subreddit_name in SUBREDDITS:
+    subreddit = REDDIT.subreddit(subreddit_name)
+    print(f'\n\n\n-----{subreddit.display_name.upper()}-----', end='\n\n\n')
+
+    for key_word in KEY_WORDS:
+        print(f'---{key_word.title()}---')
+        for search_result in subreddit.search(key_word):
+            print(f'\t{search_result.title}')
+
+        print()
+
+    print(end='\n\n')
