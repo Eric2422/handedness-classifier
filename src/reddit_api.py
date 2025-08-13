e9abe64a2d5165fb5ec62671aecd806521e5121c
Eric Zhang <ericbkzhang@gmail.com>
2025-08-13 12:41:23 -0400

    Separate image retrieval into its own function.
    The function is called `read_image_from_url()`.
    It accepts a string as a URL and returns a PIL image.

--- src/reddit_api.py
+++ src/reddit_api.py
@@ -2,11 +2,40 @@ import configparser
 import csv
 import sys
 
-import  http.client
+import http.client
 import io
 import PIL.Image
 import praw
 import requests
+
+
+def read_image_from_url(url: str) -> PIL.Image.Image:
+    """_summary_
+
+    Parameters
+    ----------
+    url : str
+        A string uniform resource locator(URL) to read the image from.
+
+    Returns
+    -------
+    PIL.Image.Image
+        The image stored at the URL, stored as a Python Image Library(PIL) image.
+
+    Raises
+    ------
+    ConnectionError
+        The image could not be read from the URL.
+    """    
+    request = requests.get(url)
+
+    # Read image
+    try:
+        return PIL.Image.open(io.BytesIO(request.content))
+
+    # Most failures seem to be from imgur, with an error of "429: Too Many Requests"
+    except PIL.UnidentifiedImageError:
+        raise ConnectionError(f'Image failed to open: {url} \nError {request.status_code}: {http.client.responses[request.status_code]}')
 
 SUBREDDITS = tuple()
 """List of subreddits to search through."""
@@ -43,7 +72,7 @@ reddit = praw.Reddit(
 for subreddit_name in SUBREDDITS:
     subreddit = reddit.subreddit(subreddit_name)
     print(f'----{subreddit.display_name.upper()}-----\n\n')
-    
+
     # Search for each given keyword
     for keyword in KEYWORDS:
         print(f'---{keyword.title()}---')
@@ -52,17 +81,11 @@ for subreddit_name in SUBREDDITS:
             url = search_result.url
             # Filter for images
             if url.endswith(IMAGE_FORMATS):
-                request = requests.get(url)
-
-                # Read image
                 try:
-                    image = PIL.Image.open(io.BytesIO(request.content))
-
-                # Most failures seem to from imgur,
-                # with an error of "429: Too Many Requests"
-                except PIL.UnidentifiedImageError:
-                    print(f'Image failed to open: {url}')
-                    print(f'Err {request.status_code}: {http.client.responses[request.status_code]}')
+                    image = read_image_from_url(url)
+                
+                except Exception as err:
+                    print(err)
 
         print()
 
