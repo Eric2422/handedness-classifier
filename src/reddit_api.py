0d75b0aa9e10d33024740bdc2bd474e630d83d7e
Eric Zhang <ericbkzhang@gmail.com>
2025-08-13 11:12:31 -0400

    Load images using Pillow. Print an error message if image fails to load.

--- src/reddit_api.py
+++ src/reddit_api.py
@@ -2,6 +2,9 @@ import configparser
 import csv
 import sys
 
+import  http.client
+import io
+import PIL.Image
 import praw
 import requests
 
@@ -36,17 +39,25 @@ reddit = praw.Reddit(
 
 for subreddit_name in SUBREDDITS:
     subreddit = reddit.subreddit(subreddit_name)
-    print(f'\n\n-----{subreddit.display_name.upper()}-----\n\n')
+    print(f'----{subreddit.display_name.upper()}-----\n\n')
 
     for keyword in KEYWORDS:
         print(f'---{keyword.title()}---')
         for search_result in subreddit.search(keyword):
-            # print(f'\t{search_result.title}')
 
             url = search_result.url
             if url.endswith(IMAGE_FORMATS):
-                print(url)
+                request = requests.get(url)
+
+                try:
+                    image = PIL.Image.open(io.BytesIO(request.content))
+
+                except PIL.UnidentifiedImageError:
+                    print(f'Image failed to open: {url}')
+                    print(f'Err {request.status_code}: {http.client.responses[request.status_code]}')
 
         print()
 
-    print('\n')
+    print()
+
+print()
